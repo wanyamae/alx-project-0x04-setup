4508ab436781faa2062bbfdc7f99d2546cbe6409
wanyamae <w.nakhanya@gmail.com>
2025-08-03 23:56:15 +0300

    Update Header component and add store functionalit

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { useCount } from "@/context/CountContext";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-  const pathname = usePathname();
-  const { count } = useCount();
+
+  const pathname = usePathname()
+  const count = useSelector((state: RootState) => state.counter.value)
+
+
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
@@ -18,22 +22,19 @@ const Header: React.FC = () => {
           {
             !["/counter-app"].includes(pathname) ? (
               <>
-                 <Button
-                  buttonLabel="Sign In"
-                  buttonBackgroundColor="red"
-                 />
-                <Button
-                  buttonLabel="Sign Up"
-                  buttonBackgroundColor="blue"
-                />
-                </>
+              <Button
+            buttonLabel="Sign In"
+            buttonBackgroundColor="red"
+          />
+          <Button
+            buttonLabel="Sign Up"
+            buttonBackgroundColor="blue"
+          /></>
             ) : (
-              <p className=" font-semibold text-lg">
-                Current count : {count}
-              </p>
+              <p className=" font-semibold text-lg">Current count : {count}</p>
             )
           }
-         
+
         </div>
       </div>
     </header>
